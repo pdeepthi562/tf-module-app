a40ce8cdde04a6940037a1c43841408af8ebe061
dpathim <dpdevops6@gmail.com>
2023-09-26 16:01:59 -0400

    Terraform

---
+++ userdata.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yum install ansible -y &>>/opt/user.data.log
+ansible-pull -i localhost, -U https://github.com/pdeepthi562/roboshop-ansible.git main.yml -e component=${component} &>>/opt/user.data.log
--- vars.tf
+++ vars.tf
@@ -7,5 +7,6 @@ variable "port" {}
 variable "sg_ingress_cidr" {}
 variable "ssh_ingress_cidr" {}
 variable "zone_id" {}
+variable "instance_type" {}
 
 
