85ba6174ea8a2c75576eae05dbc775edc15e98d5
dpathim <dpdevops6@gmail.com>
2023-09-27 15:56:49 -0400

    Terraform

--- vars.tf
+++ vars.tf
@@ -11,7 +11,7 @@ variable "instance_type" {}
 variable "desired_capacity" {}
 variable "max_size" {}
 variable "min_size" {}
-variable "alb_name" {}
-variable "listener" {}
+variable "private_alb_name" {}
+variable "private_listener" {}
 variable "lb_priority" {}
 
