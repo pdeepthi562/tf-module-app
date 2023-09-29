b83a088b694f2b5c00a2cd00071490fa4bfaae6c
dpathim <dpdevops6@gmail.com>
2023-09-29 15:59:21 -0400

    Terraform

--- main.tf
+++ main.tf
@@ -117,23 +117,23 @@ resource "aws_lb_target_group_attachment" "public" {
   availability_zone = "all"
 
 }
-#
-#resource "aws_lb_listener_rule" "public" {
-#  count        = var.component == "frontend" ? 1 : 0
-#  listener_arn = var.public_listener
-#  priority     = var.lb_priority
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.public[0].arn
-#  }
+
+resource "aws_lb_listener_rule" "public" {
+  count         = var.component == "frontend" ? 1 : 0
+  listener_arn  = var.public_listener
+  priority      = var.lb_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.public[0].arn
+  }
 
 
-#  condition {
-#    host_header {
-#      values = ["${var.env}.pdevops562.online"]
-#    }
-#  }
-#}
+  condition {
+    host_header {
+      values = ["${var.env}.pdevops562.online"]
+    }
+  }
+}
 
 
